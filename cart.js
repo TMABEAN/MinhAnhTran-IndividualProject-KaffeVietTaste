84b7289cd0c71f02fee11d77a0f58735bba090b4
TMABEAN <63044799+TMABEAN@users.noreply.github.com>
2026-03-12 03:32:14 +0100

    Add reading data localStorage

--- cart.js
+++ cart.js
@@ -1,1 +1,7 @@
+/* =========================================
+Read cart data from localStorage
+========================================= */
+function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+} // If nothing exists yet, return empty array
 
